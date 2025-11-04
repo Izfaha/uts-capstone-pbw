99a99bffc6682a1534c0a9cb192a19a64810867e
Izfaha <rasberry006@gmail.com>
2025-11-04 17:16:44 +0700

    update

--- script.js
+++ script.js
@@ -11,46 +11,58 @@ function tampilWaktu() {
 
 
 document.addEventListener("DOMContentLoaded", () => {
+  // ambil elemet lewat id='dark-mode'
   const darkIcon = document.getElementById('dark-mode');
+  // ambil elemet lewat id='light-mode'
   const lightIcon = document.getElementById('light-mode');
 
-  // DARK MODE
+  // Darkmode pake evenListener
   darkIcon.addEventListener('click', () => {
+    // ini hanya keperluan untuk debug saja
     console.log("Dark Mode clicked!");
 
-    // HERO
+    // ini darkmode di bagian "HERO"
     const sectionHero = document.getElementById('hero');
     sectionHero.classList.remove('bg-danger-subtle');
     sectionHero.classList.add('bg-dark-subtle');
 
-    // ARTICLE
+    // ini darkmode di bagian "ARTICLE"
     const bgArticle = document.getElementById('article');
     bgArticle.classList.remove('bg-light', 'bg-secondary-subtle');
     bgArticle.classList.add('bg-dark', 'text-light');
 
-    // GALLERY
+    // ini darkmode di bagian "GALLERY"
     const bgGallery = document.getElementById('gallery');
     bgGallery.classList.remove('bg-danger-subtle');
     bgGallery.classList.add('bg-dark-subtle');
 
-    // FOOTER
+    // ini darkmode di bagian "SCHEDULE"
+    const schedule = document.getElementById('schedule');
+    schedule.classList.add('bg-dark-subtle');
+
+    // ini darkmode di bagian "FOOTER"
     const footer = document.querySelector('footer');
     footer.classList.add('bg-dark', 'text-light');
 
-    // ICONS FOOTER
+    // ini darkmode di bagian "ICONS FOOTER"
     document.querySelectorAll('.icons').forEach(icon => {
       icon.classList.remove('text-dark');
       icon.classList.add('text-light');
     });
   });
 
-  // LIGHT MODE
+  // ini untuk LIGHT MODE
   lightIcon.addEventListener('click', () => {
     console.log("Light Mode clicked!");
 
     // HERO
     const sectionHero = document.getElementById('hero');
-    sectionHero.classList.remove('bg-body-tertiary');
+    /*
+      pada bagian ini saya mendapat suatu anomali yaitu setelah saya
+      klik dark mode dan kembali ke light mode, backgroun di hero ndak mau berubah menjadi 
+      pink lagi. Ternyata saya belum remove class sebelumnya
+    */
+    sectionHero.classList.remove('bg-dark-subtle');
     sectionHero.classList.add('bg-danger-subtle');
 
     // ARTICLE
@@ -62,6 +74,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const bgGallery = document.getElementById('gallery');
     bgGallery.classList.remove('bg-dark-subtle');
     bgGallery.classList.add('bg-danger-subtle');
+
+    // light mode untuk "SCHEDULE"
+    const schedule = document.getElementById('schedule');
+    schedule.classList.remove('bg-dark-subtle');
 
     // FOOTER
     const footer = document.querySelector('footer');
